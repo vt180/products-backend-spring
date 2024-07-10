4b6f9f6b239ebb259d53e353c7d6a586639d47d4
Robin Mohr <mail@robinmohr.de>
2024-07-10 17:53:55 +0200

    Enable CORS for ProductController
    A CrossOrigin annotation has been added to the ProductController class. This change allows CORS requests from the specified origin, which in this case is "http://localhost:5173". This is crucial for enabling cross-domain requests, helping with the integration and interaction of different services.

--- src/main/java/de/robinmohr/backend/product/ProductController.java
+++ src/main/java/de/robinmohr/backend/product/ProductController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@CrossOrigin("http://localhost:5173")
 public class ProductController {
 
     private final ProductService productService;
